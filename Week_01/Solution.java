facd3ac9577a83cd313b293c1815f3ad1265fd2a
jadechina <starty@live.cn>
2020-06-26 21:58:38 +0800

    算法训练营11期2班第一周作业

---
+++ Week_01/Solution.java
@@ -0,0 +1,117 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+
+    /**
+     * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     */
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i != index) {
+                    nums[index] = nums[i];
+                    nums[i] = 0;
+                }
+                index++;
+            }
+        }
+    }
+
+    /**
+     * 
+     * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线
+     * i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+     * 说明：你不能倾斜容器，且 n 的值至少为 2。
+     */
+    public int maxArea(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int max = 0;
+        for (int i = 0, j = height.length - 1; i < j;) {
+            int minHeight = height[i] < height[j] ? height[i++] : height[j--];
+            max = Math.max(max, (j - i + 1) * minHeight);
+        }
+        return max;
+    }
+
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     */
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int com = target - nums[i];
+            if (map.containsKey(com)) {
+                return new int[] { map.get(com), i };
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+    /**
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？ 注意：给定 n 是一个正整数。
+     */
+    public int climbStairs(int n) {
+        if (n < 3) {
+            return n;
+        }
+        int f1 = 1;
+        int f2 = 2;
+        int f3 = 3;
+        for (int i = 4; i <= n; i++) {
+            f1 = f2;
+            f2 = f3;
+            f3 = f1 + f2;
+        }
+        return f3;
+    }
+
+    /**
+     * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0
+     * 请你找出所有满足条件且不重复的三元组。 注意：答案中不可以包含重复的三元组。
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3)
+            return result;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0)
+                break;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int target = -nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    result.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                } else if (nums[left] + nums[right] < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+
+}
