619cfce19afff90e2ef4ed638cf873b34571f7d4
shentao <starty@live.cn>
2020-07-04 14:11:07 +0800

    二叉树实现

---
+++ Week_02/TreeNode.java
@@ -0,0 +1,24 @@
+/**
+ * yingyinglicai.com Inc.
+ * Copyright (c) 2013-2020 All Rights Reserved.
+ */
+
+/**
+ * 二叉树实现
+ *
+ * @author tao.shen
+ * @version : TreeNode, v1.0 2020年07月04日 12:06 下午 tao.shen Exp $
+ */
+public class TreeNode {
+
+    int      val;
+
+    TreeNode left;
+
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
